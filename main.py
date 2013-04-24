a355582786e40d88a8ee38dc245616649bdd860e
Masato Yoshimi <myoshimi@minilab.biz>
2013-04-25 08:07:22 +0900

    modify bugs
    modify bugs

--- main.py
+++ main.py
@@ -28,11 +28,19 @@ def format_ba(a, m, d):
         d -= 1
     return x
 
+def hamming_output(in_hamming, minterm, blength):
+    for idx in xrange(len(in_hamming)):
+        print "["+str(idx)+"]"
+        for m in in_hamming[idx]:
+            print "\t "+format_ba(minterm[m['idx'][0]], m['mask'], blength)
+        print "------------------------------------------------------------"
+        
+
 def compute_stage(in_hamming, minterm):
     out_hamming = [[] for i in xrange(len(in_hamming))]
     primes = []
     for idx in xrange(len(in_hamming)-1):
-        print "idx",idx
+        # print "idx",idx
         for s in in_hamming[idx]:
             for d in in_hamming[idx+1]:
                 # hd: ハミング距離
@@ -46,11 +54,6 @@ def compute_stage(in_hamming, minterm):
                               | s['mask'] | d['mask']
                     # cb : dont careを除くビットの数
                     cb = count_bit(minterm[s['idx'][0]]|dc) - count_bit(dc)
-                    
-                    print " s",format(minterm[s['idx'][0]],'0'+str(blength)+'b'),
-                    print " d",format(minterm[d['idx'][0]],'0'+str(blength)+'b'),
-                    print " hamming:",hd,
-                    print " dc",format(dc,'0'+str(blength)+'b'),"cb:",cb
 
                     nidx = s['idx']+d['idx']
                     nidx.sort()
@@ -58,17 +61,17 @@ def compute_stage(in_hamming, minterm):
                     s['flag'] += 1
                     d['flag'] += 1
                     if ndic not in out_hamming[cb]:
-                        print "append",ndic
                         out_hamming[cb].append(ndic)
-    for idx in range(len(in_hamming)):
+    for idx in xrange(len(in_hamming)):
         for s in in_hamming[idx]:
             if s['flag'] == 0:
-                print "result",
                 primes.append(s)
-                print s, "*"
+    hamming_output(out_hamming, minterm, blength)
+    print "Primes"
+    for p in primes:
+        print "\t "+format_ba(minterm[p['idx'][0]], p['mask'], blength)
+    print "------------------------------------------------------------"
     return (out_hamming, primes)
-
-
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-o', nargs='*')
@@ -95,39 +98,25 @@ print "======================== Computing ========================="
 minterm = minterm_true + minterm_dc
 blength= len(format(max(minterm),'0b')) # ビット長の計算
 print "blength:", blength
+print "# of 1  :"+str(len(minterm_true))
+print "# of DC :"+str(len(minterm_dc))
 
-"""
-i = 0
-for n in minterm_true:
-    print format(i, '4d')+" : "+ \
-        format(n, '0'+str(blength)+'b') + " : " + str(n)
-    i = i+1
-for n in minterm_dc:
-    print format(i,'4d')+" : "+ \
-        format(n, '0'+str(blength)+'b') + " : " + str(n)
-    i = i+1
-"""
 print "======================== Input Data ========================"
 
 primes = []
 hamming = [[] for i in range(blength+1)]
 
-i=0
-for n in minterm:
-    # bc : ビットカウント
-    bc = count_bit(n)
-    hamming[bc].append({"idx":[i], "mask":0, "flag":0})
-    i=i+1
+# Generating dictionary for each input
+# 
+for n in xrange(len(minterm)):
+    hamming[count_bit(minterm[n])].append({"idx":[n], "mask":0, "flag":0})
 
-n=0
-for h in hamming:
-    print "["+str(n)+"]"
-    for m in h:
+for h in xrange(len(hamming)):
+    print "["+str(h)+"]"
+    for m in hamming[h]:
         print "\t m"+str(minterm[m['idx'][0]])+"\t "+\
             format(minterm[m['idx'][0]], '0'+str(blength)+'b')
     print "------------------------------------------------------------"
-    n += 1
-
 
 print "=============== Mid Process Computation ===================="
 pp = 0
@@ -137,12 +126,11 @@ while 1:
     primes.extend(hamming_tuple[1])
     n = 0
     for h in hamming2:
-        print h
         n += len(h)
-        i=i+1
     print "remains : ", n
     hamming = hamming2
     pp += 1
+
     if n == 0:
         break
 
@@ -150,62 +138,55 @@ print "======================== Prime Expression ========================"
 i = 0
 for p in primes:
     print format(i,'4d'),\
-        format_ba(minterm[p['idx'][0]], p['mask'], blength), \
-        format(minterm[p['idx'][0]], '5d'), format(p['mask'], '08b')
+        format_ba(minterm[p['idx'][0]], p['mask'], blength)
     i+=1
 
+
+# Generating btable;    
+print "======================== Tables ========================"
 btable = [0 for i in xrange(len(minterm_true))]
-for j in range(len(minterm_true)):
+
+for j in xrange(len(minterm_true)):
     btable[j] = 0
     for p in primes:
         btable[j] *= 2
         if j in p['idx']:
             btable[j] += 1
 
+for bt in xrange(len(btable)):
+    print "p["+str(bt)+"]\t"+str(minterm_true[bt])+"\t"+\
+        format(btable[bt],'0'+str(len(primes))+'b')
+            
 # 被覆問題
 g = [btable[0]]
 r = []
 
-for bt in btable:
-    print "bt : "+format(bt,'0'+str(len(primes))+'b')
+for bt in xrange(len(btable)):
     r = []
     for ib in reversed(xrange(len(primes))):
-        eidx = bt&(2**ib)
+        eidx = btable[bt]&(2**ib)
         if eidx:
-            #print "  "+format(eidx,'0'+str(blength)+'b')
             for p in g:
                 r.append(p|eidx)
+    g = list(set(r))
 
-    g = []
-    for p in r:
-        if p not in g:
-            g.append(p)
-    """
-    print " r[",
-    for i in r:
-        print format(i,'0'+str(blength)+'b')+",",
-    print "]"
-    print " g[",
-    for i in g:
-        print format(i,'0'+str(blength)+'b')+",",
-    print "]"
-    """
- 
-print " g[",
+
+g.sort(cmp=lambda x,y: cmp(count_bit(x), count_bit(y)))
+
+print "========= Prime Combinations for Logical equations ========="
 for i in g:
     print format(i,'0'+str(len(primes))+'b')+","+str(count_bit(i))
-print "]"
 
 # 解の生成
 result = []
 for r in g:
     rtmp = []
     for ib in reversed(xrange(len(primes))):
-        if r & 2**ib :
-            pidx = len(primes)-1-ib
-            midx = primes[pidx]['idx'][0]
-            mask = primes[pidx]['mask']
-            rtmp.append(format_ba(minterm[midx],mask, blength))
+        if r & (2**ib) :
+            pidx = len(primes)-ib-1
+            rtmp.append(format_ba(primes[pidx]['idx'][0],
+                                  primes[pidx]['mask'],
+                                  blength))
     result.append(rtmp)
 
 min_num   = sys.maxint
@@ -215,6 +196,13 @@ for r in result:
         min_num = len(r)
         min_array = r
 
-print "Number of Equations : "+str(len(r))
-print r
+print "Number of Equations : "+str(min_num)
+for i in r:
+    print i,
+print ""
+
+
+#if __name__ == '__main__':
+
+
 
